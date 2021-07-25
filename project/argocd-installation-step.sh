80f4cb49718e7b08c20b24d3e1c173d2ec97186c
Subhas Bhat <bhatsubhas@gmail.com>
2021-07-26 01:02:14 +0530

    Add ArgoCD installation steps file

---
+++ project/argocd-installation-step.sh
@@ -0,0 +1,7 @@
+# ArgoCD installation steps
+kubectl create namespace argocd
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+
+# Command to get the ArgoCD password (username for this is admin)
+kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
+
